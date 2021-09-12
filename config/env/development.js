3109e2e6c25a9165a804981ed9ede1099d48e39a
Bigyan Shrestha <s.bigee@gmail.com>
2021-09-12 18:16:47 -0500

    config params per env

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+module.exports = {
+    
+    // api configuration parameters
+    api: {
+        url: "https://www.materialsproject.org/rest/v2/",
+        key: "wa1tsIbKzQCDqDoqiq"
+    }
+
+}
--- config/env/production.js
+++ config/env/production.js
@@ -392,6 +392,12 @@ module.exports = {
 
   },
 
+  // api configuration parameters
+  api: {
+      url: "https://www.materialsproject.org/rest/v2/",
+      key: "wa1tsIbKzQCDqDoqiq"
+  }
+
 
 
 };
