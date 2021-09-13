7ad2e262ffee0c0ea7d8d17bc3ae31d5b755e45a
Bigyan Shrestha <s.bigee@gmail.com>
2021-09-13 10:04:45 -0500

    added search and started details view

--- api/controllers/MaterialController.js
+++ api/controllers/MaterialController.js
@@ -12,39 +12,44 @@
  
  module.exports = {    
      
-     async fetchMaterials(req, res) {
-         try {     
-             let {id} = req.allParams();    
- 
-             if (!id) {
-                 sails.log.error("Id param is not provided.");
-                 return res.badRequest({
-                     err: "You must provide either the material id " +
-                         "or the material formula in the ID request parameter."
-                 })
-             }
- 
-             const endpoint = `${baseUrl}/materials/${id}/vasp`
-             sails.log.debug(endpoint);
- 
-             const result = await axios.get(endpoint, {
-                 headers: {
-                     'X-API-KEY': apiKey
-                 }
-             });
-             
-             const data = await sails.helpers
-                 .materialTransform(result.data.response);
- 
-             return res.ok(data);
- 
-         } catch (err) {
-             sails.log.error(err);
-             return res.serverError({
-                 err: "Something bad happend in our servers. Please contact the support."
-             });
-         }
-     },
+    async fetchMaterials(req, res) {
+        try {     
+            let {id} = req.allParams();    
+
+            if (!id) {
+                sails.log.error("Id param is not provided.");
+                return res.badRequest({
+                    err: "You must provide either the material id " +
+                        "or the material formula in the ID request parameter."
+                })
+            }
+
+            const endpoint = `${baseUrl}/materials/${id}/vasp`
+            sails.log.debug(endpoint);
+
+            const result = await axios.get(endpoint, {
+                headers: {
+                    'X-API-KEY': apiKey
+                }
+            });
+            
+            const data = await sails.helpers
+                .materialTransform(result.data.response);
+
+            return res.ok(data);
+
+        } catch (err) {
+            sails.log.error(err);
+            return res.serverError({
+                err: "Something bad happend in our servers. Please contact the support."
+            });
+        }
+    },
+
+    getMaterialDetails(req, res) {
+        return res.view("pages/materialdetails");
+    }
+
  
  };
  
